3b8c5e450942a1c3f697f0b5a89e20d933bf003d
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-12 13:18:24 -0400

    update

--- src/hasTeen.java
+++ src/hasTeen.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class hasTeen {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+
+        System.out.println(isTeen(9));
+        System.out.println(isTeen(13));
+
+    }
+
+    private static boolean hasTeen(int num1, int num2, int num3) {
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
+    }
+
+    public static boolean isTeen(int num) {
+        return num >= 13 && num <= 19;
+    }
+
 }
